541f7d954b48c2919215f015ec9528f3036cc17a
christianamaury <chris.amaury@hotmail.com>
2025-05-11 17:21:35 -0700

    Extracting informaiton from the map variable - game, description and title

--- App.js
+++ App.js
@@ -33,11 +33,22 @@ export default function App() {
       {/* Mapping Games Variable*/}
       {games.map(game => (
         <View key={game.slug} style={styles.card}>
-          <Image source={{uri: game.image}} style={{
-          width: 107, 
-          height: 147, 
-          borderRadius: 10,
-        }}>
+          <Image source={{uri: game.image}} style={styles.image}>
+
+            <Text style={styles.title}>
+              {/* Game Variable Title: */}
+              {game.title}
+            </Text>
+
+            <Text style={styles.description}>
+            {/* Game Description Variable: */}
+            {game.description}
+            </Text>
+
+            <Text style={styles.score}>
+                {/* Game Score Variable: */}
+                {game.score}
+            </Text>
           </Image>
         </View>
       ))}
@@ -57,10 +68,35 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#3f4952',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+
+  },
+
+  description: {
+
+  },
+
+  score: {
+
+  },
+
+  card: {
+
+  },
+
+//Elements settings for the Image;
+  image: {
+      width: 107, 
+      height: 147, 
+      borderRadius: 10, 
+  }
+ 
 });
