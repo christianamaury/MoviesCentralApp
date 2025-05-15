70325bc949880400a9e87e4f12bd451dfe954cae
christianamaury <chris.amaury@hotmail.com>
2025-05-15 08:27:27 -0700

    Adding Safearea component to the main View Component

--- App.js
+++ App.js
@@ -28,10 +28,16 @@ export default function App() {
   return (
 
     <View styles={styles.container}>
+    
+      {/*Adding Safearea Component*/}
+      <SafeAreaView> 
+
       {/* //Adding Script View Component so users can scroll in the App;  */}
       <ScrollView>
+
           {/* This statusBar is a Component that's not from React Native.  */}
           <StatusBar style='light'/>
+
           {/* Mapping Games Variable*/}
           {games.map(game => (
             <View key={game.slug} style={styles.card}>
@@ -51,9 +57,11 @@ export default function App() {
                     {/* Game Score Variable: */}
                     {game.score}
                 </Text>
+                
               </Image>
             </View>
           ))}
+
             {/* Image from the network; */}
             {/* <Image source={{uri: 'https://www.google.com/imgres?q=movies%20caratulas%20en%20ingles&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D5241068029321596&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D5241068029321596%26id%3D887208618040914%26set%3Da.887220101373099&docid=agssFYNBnpBazM&tbnid=GJj8K1q3_N5D2M&vet=12ahUKEwjK187t_LeNAxXJPUQIHVn8ILMQM3oECFUQAA..i&w=2048&h=1375&hcb=2&ved=2ahUKEwjK187t_LeNAxXJPUQIHVn8ILMQM3oECFUQAA'}}
               style={{width: 215, height: 294}} /> */}
@@ -63,9 +71,11 @@ export default function App() {
             {/* <Button title='Start Now' onPress={() => {alert('Welcome!')}}/> */}
 
             {/* <StatusBar style="auto" /> */}
+
     </ScrollView>
+   </SafeAreaView>
   </View>
-  
+
   );
 }
 
